cd5e82fdb84959b3a92961b26434d198e72c933f
pawel ch <not_provided>
2015-03-14 17:50:55 +0000

    better way of loading files

--- run.r
+++ run.r
@@ -1,5 +1,34 @@
-setwd("/home/pawel/kaggle/kaggle-cyclists/")
-source("code/funcs.r")
+#clear existing elemts - comment this out if needed
+rm(list = ls())
 
-#cannot upload dataset into repository due to license limitation
-t=load_testing_file("/home/pawel/kaggle/datasets/kaggle-cyclists/")
+#flags for file reading
+f_te=0
+f_tr=0
+f_sa=0
+f_f=0
+
+d_kaggle="/home/pawel/kaggle/kaggle-cyclists/"                                    # working directory
+d_test_file="/home/pawel/kaggle/datasets/kaggle-cyclists/test.csv"                # test file
+d_train_file="/home/pawel/kaggle/datasets/kaggle-cyclists/train.csv"              # train file
+d_sample_file="/home/pawel/kaggle/datasets/kaggle-cyclists/sampleSubmission.csv"  # sample file
+
+
+# set the working directory to the required directory
+ setwd(d_kaggle)
+
+#load needed functions
+ source("code/funcs.r")
+
+# it is not allowed to upload dataset into repository due to license limitation
+# therefore, we have to load them from the local repository, sorry
+# files can be obtained from http://www.kaggle.com/c/bike-sharing-demand/data
+
+#load testing file
+ f_test=load_testing_file( P_file_name = d_test_file, P_test =f_te )
+
+#load training file
+ f_train=load_testing_file( P_file_name = d_train_file, P_test =f_tr )
+
+#load sample file
+ f_sample=load_testing_file( P_file_name = d_sample_file, P_test = f_sa )
+
