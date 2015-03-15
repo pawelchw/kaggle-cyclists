27fcfc2803429f4212edf93244228a3384bdcf59
pawel ch <not_provided>
2015-03-15 11:25:07 +0000

    preparing for running simple knn

--- run.r
+++ run.r
@@ -44,6 +44,28 @@ d_sample_file="/home/pawel/kaggle/datasets/kaggle-cyclists/sampleSubmission.csv"
                               , P_column_name = "day")
 
 #add hour column
-f_train=add_column_into_frame( P_data_frame = f_train
-                               , P_column_vector = f_train[ ,c("datetime")]
-                               , P_column_name = "hour")
+ f_train=add_column_into_frame( P_data_frame = f_train
+                              , P_column_vector = hours(f_train[ ,c("datetime")])
+                              , P_column_name = "hour")
+
+#add day column
+ f_test=add_column_into_frame( P_data_frame = f_test
+                             , P_column_vector = weekdays(as.Date(f_test$datetime))
+                             , P_column_name = "day")
+
+#add hour column
+ f_test=add_column_into_frame( P_data_frame = f_test
+                             , P_column_vector = hours(f_test[ ,c("datetime")])
+                             , P_column_name = "hour")
+
+
+#training data for knn
+knn_train = f_train[ ,c("season","holiday","workingday","day","hour","temp","casual","registered")]
+#response data for knn
+knn_response = factor(f_train[ ,"count"])
+#test data for knn
+knn_test = f_train[ ,c("season","holiday","workingday","day","hour","temp","casual","registered")]
+
+
+
+#a=knn(knn_train, knn_test, knn_response, k = 112, prob=TRUE)
