9fb202b3b0cc09d4a1d6e2041737ea582c1ec559
pawel ch <not_provided>
2015-03-15 11:24:40 +0000

    extending data frame manipulation

--- code/funcs.r
+++ code/funcs.r
@@ -26,6 +26,38 @@
                                     , P_column_name # name of the new column
                                     )
    {
-     P_data_frame[, P_column_name] = P_column_vector
+     #check if string/characters
+     if (typeof(P_column_vector) %in% c("character","string"))
+     {
+        l_df = prep_lookup( P_column_vector = P_column_vector
+                          , P_column_name = P_column_name)  
+        P_data_frame[, P_column_name] = match(P_column_vector,l_df$P_column_name)
+     }
+     # just add it
+     else
+     {
+        P_data_frame[, P_column_name] = P_column_vector 
+     }
      return (P_data_frame)
    }
+   
+   
+# this function prepares lookup table for unique string values
+   prep_lookup <- function( P_column_vector # vector of strings; each string will get a unique no
+                          , P_column_name # name used inside data frame
+                          )
+   {
+     #apparently, this is the fastest method of converting a vector of strings
+     #into unique values. see
+     # http://stackoverflow.com/questions/3879522/finding-unique-values-from-a-list
+     # for details
+     
+     # create data frame with a columd day of unique days
+     df = data.frame( P_column_name = unique(unlist(P_column_vector, use.names = FALSE)))
+     # extend by adding unique numeric value
+     df[, "id"]=seq(from = 1,to = dim(df)[1])
+     return (df)
+   }
+   
+   
+# we need to transform strings into ids
