507e06ed465003f30ca2a3d8adb2ee3b2ed7eb56
pawel ch <not_provided>
2015-03-18 07:49:13 +0000

    formatting data for training, adding cubit functions

--- run.r
+++ run.r
@@ -7,10 +7,10 @@ library("chron")
 rm(list = ls())
 
 #flags for file reading
-f_te=0
-f_tr=0
-f_sa=0
-f_f=0
+t_te=0
+t_tr=0
+t_sa=0
+t_f=0
 
 d_kaggle="/home/pawel/kaggle/kaggle-cyclists/"                                    # working directory
 d_test_file="/home/pawel/kaggle/datasets/kaggle-cyclists/test.csv"                # test file
@@ -29,13 +29,13 @@ d_sample_file="/home/pawel/kaggle/datasets/kaggle-cyclists/sampleSubmission.csv"
 # files can be obtained from http://www.kaggle.com/c/bike-sharing-demand/data
 
 #load testing file
- f_test=load_testing_file( P_file_name = d_test_file, P_test =f_te )
+ f_test=load_testing_file( P_file_name = d_test_file, P_test =t_te )
 
 #load training file
- f_train=load_testing_file( P_file_name = d_train_file, P_test =f_tr )
+ f_train=load_testing_file( P_file_name = d_train_file, P_test =t_tr )
 
 #load sample file
- f_sample=load_testing_file( P_file_name = d_sample_file, P_test = f_sa )
+ f_sample=load_testing_file( P_file_name = d_sample_file, P_test = t_sa )
 
 
 #add day column
@@ -59,13 +59,13 @@ d_sample_file="/home/pawel/kaggle/datasets/kaggle-cyclists/sampleSubmission.csv"
                              , P_column_name = "hour")
 
 
-#training data for knn
-knn_train = f_train[ ,c("season","holiday","workingday","day","hour","temp","casual","registered")]
-#response data for knn
-knn_response = factor(f_train[ ,"count"])
-#test data for knn
-knn_test = f_train[ ,c("season","holiday","workingday","day","hour","temp","casual","registered")]
+#training data
+f_train_formatted = f_train[ ,c("season","holiday","workingday","day","hour","temp")]
+#response data
+response = as.integer(factor(f_train[ ,"count"]))
+#test data
+f_test_formatted = f_test[ ,c("season","holiday","workingday","day","hour","temp")]
 
+fit=fit_with_cubist(P_train = f_train_formatted, P_response = response)
+predict=predict_with_cubist(P_fit = fit,P_test = f_test_formatted)
 
-
-#a=knn(knn_train, knn_test, knn_response, k = 112, prob=TRUE)
